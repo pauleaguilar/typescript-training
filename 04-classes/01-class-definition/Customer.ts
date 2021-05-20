f4eacace76dc5863c18b32c0cdb68742baede357
Paul Aguilar <pauleaguilar@gmail.com>
2021-05-20 09:33:36 -0500

    added classes and constructors lesson

---
+++ 04-classes/01-class-definition/Customer.ts
@@ -0,0 +1,19 @@
+class Customer {
+
+    firstName: string;
+    lastName: string;
+
+    constructor(theFirst: string, theLast: string) {
+        this.firstName = theFirst;
+        this.lastName = theLast;
+    }
+}
+
+
+
+let myCustomer = new Customer("Martin", "Lopez"); 
+
+
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
